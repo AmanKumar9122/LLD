c9e2a15d3dcb51a1513fd6120afe5ac0c5bf360c
AmanKumar9122 <amanraj15112003@gmail.com>
2025-06-12 09:13:55 +0530

    Example 3 SRP

---
+++ src/SimpleResponsibilitySystem/Example3/EmployeePerformanceGenerator.java
@@ -0,0 +1,12 @@
+package SimpleResponsibilitySystem.Example3;
+
+public class EmployeePerformanceGenerator {
+    public String generateReport(String reportType, Employee employee) {
+        if (reportType.equals("PDF")) {
+            return "Generating PDF report.";
+        } else if (reportType.equals("Word")) {
+            return "Generating Word report.";
+        }
+        return "Report type not supported.";
+    }
+}
