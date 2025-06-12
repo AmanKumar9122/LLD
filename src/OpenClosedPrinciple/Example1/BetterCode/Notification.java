0bb1b04e480be470136df7d84c607d2ba10d52c9
AmanKumar9122 <amanraj15112003@gmail.com>
2025-06-12 09:37:27 +0530

    Better Code OCP

---
+++ src/OpenClosedPrinciple/Example1/BetterCode/Notification.java
@@ -0,0 +1,5 @@
+package OpenClosedPrinciple.Example1.BetterCode;
+
+public class Notification {
+    void send();
+}
---
+++ src/OpenClosedPrinciple/Example1/BetterCode/NotificationSender.java
@@ -0,0 +1,10 @@
+package OpenClosedPrinciple.Example1.BetterCode;
+
+import java.util.*;
+public class NotificationSender {
+    public void sendNotifications(List<Notification> notifications) {
+        for(Notification notification : notifications) {
+            notification.send();
+        }
+    }
+}
